8dcf0fbb80e176a319e2b13b5e4976846ba165df
Katherine Nguyen <nguyenkat01@yahoo.com>
2020-12-17 20:08:25 -0800

    pull latest

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
