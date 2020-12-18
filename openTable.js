17be60df3990a2d2dcacd1c18d487de6a04d71b0
Markos Rafael <escalante.markos18@gmail.com>
2020-12-17 20:46:45 -0800

    create waiting table and open table js files.

---
+++ openTable.js
@@ -0,0 +1,12 @@
+const openTable = [
+    {
+        customerName: "Ya boi",
+        customerEmail: "yb@gmail.com",
+        customerId: "where is my pull request",
+        phoneNumber: "855-555-5555",
+
+    }
+
+];
+
+module.exports = openTable;
